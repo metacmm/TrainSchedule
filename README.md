31a3f5bc4eb0039aabd0bbecd17d3fa0d6546cc8
Xiaojie Liu <42020550+metacmm@users.noreply.github.com>
2019-10-09 14:25:14 -0700

    add readme.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # TrainSchedule
+
+## Overview
+
+This is a web site providing up-to-date information about various trains, namely their arrival times and how many minutes remain until thry arrive at their station.
+
+## Instruction
+
+* User is able to submit Train Name, Destination, First Train Time (in military time) and Frequency.
+
+* All the train data will be store permanently and can be edited and deleted.
+
+* The next train time and minutes to arrival will be updated once every minute.
--- assets/script/app.js
+++ assets/script/app.js
@@ -4,10 +4,12 @@ $(document).ready(function () {
     database = firebase.database();
     readTrainInfoFromDB();
 
+    /**event handler while clicking buttons */
     $(document).on("click", ".btn-edit", editTrainInfo);
     $(document).on("click", ".btn-delete", removeTrainInfo);
     $("#btn-submit").on("click", saveTrainInfoToDB);
     $("#btn-cancel").on("click", resetInputForm);
 
+    /**refresh the train info every minute */
     var intervalId = setInterval(readTrainInfoFromDB, 60000);
 });
